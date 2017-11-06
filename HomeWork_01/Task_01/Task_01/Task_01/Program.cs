98b39b845ada6d1e53c81952d0aaea8de3f2955c
KamiJal <ushurbakiev.k@gmail.com>
2017-11-06 17:14:51 +0600

    Task 01 Ready

---
+++ HomeWork_01/Task_01/Task_01/Task_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string name;
+                Console.Clear();
+                Console.WriteLine("Пожалуйста введи свое имя!");
+                name = Console.ReadLine();
+
+                name = name.Trim();
+
+                if (!String.IsNullOrWhiteSpace(name))
+                    Console.WriteLine("\nПриветствую тебя - " + name);
+                else
+                    Console.WriteLine("\nПопробуй еще раз. Ты ведь ничего не ввел!");
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
